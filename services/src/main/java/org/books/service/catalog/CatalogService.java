730abffbc92b29cc890e4118b456ac4655390608
beeet <beeet@192.168.0.102>
2014-02-19 18:43:39 +0100

    Revert "maxResults added to CatalogService"
    This reverts commit 9ba290babd08dbf691bbbd9c5d89f96495aeae34.

--- services/src/main/java/org/books/service/catalog/CatalogService.java
+++ services/src/main/java/org/books/service/catalog/CatalogService.java
@@ -6,13 +6,11 @@ import javax.ejb.Remote;
 import org.books.persistence.catalog.Book;
 
 @Remote
-public interface CatalogService extends Serializable {
+public interface CatalogService extends Serializable{
 
-    public void addBook(Book book) throws BookAlreadyExistsException;
+	public void addBook(Book book) throws BookAlreadyExistsException;
 
-    public Book findBook(String isbn) throws BookNotFoundException;
+	public Book findBook(String isbn) throws BookNotFoundException;
 
-    public List<Book> searchBooks(String... keywords);
-
-    public List<Book> searchBooks(int maxResults, String... keywords);
+	public List<Book> searchBooks(String... keywords);
 }
