14738ec99b83f05cea34307df5ea76e84e796184
Markus Tanner <mactanner@gmail.com>
2014-02-22 19:56:43 +0100

    unnecessary "extends Serializable" removed

--- services/src/main/java/org/books/service/catalog/CatalogService.java
+++ services/src/main/java/org/books/service/catalog/CatalogService.java
@@ -6,11 +6,11 @@ import javax.ejb.Remote;
 import org.books.persistence.catalog.Book;
 
 @Remote
-public interface CatalogService extends Serializable{
+public interface CatalogService {
 
-	public void addBook(Book book) throws BookAlreadyExistsException;
+    public void addBook(Book book) throws BookAlreadyExistsException;
 
-	public Book findBook(String isbn) throws BookNotFoundException;
+    public Book findBook(String isbn) throws BookNotFoundException;
 
-	public List<Book> searchBooks(String... keywords);
+    public List<Book> searchBooks(String... keywords);
 }
